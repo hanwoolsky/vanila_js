52b0fb7478216cfce9456004827fddfbfe21b2ad
Hanwool Huh <hhw0925@naver.com>
2021-01-06 02:12:07 +0900

    [Update] eventhandler & if, else.

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-console.log('Im Working. Im JS. Im Beautiful');
+//const title = document.getElementById("title");
+const title = document.querySelector("#title");
 
-//  list = []
-//  object = {}
-
-const list = ["Mon", "Tue", "Wed"];
-
-const MyInfo = {
-    name: "hanwool",
-    age: 24,
-    gender: "female",
-    FavMovies: ["Frozen", "3 idio"],
-    FavFood: [
-        {
-            name: "galbicchim",
-            type: "Meat"
-        },
-        {
-            name: "chicken",
-            fatty: true
-        }
-    ]
+function handleResize(){
+    console.log("I have been resized")
 }
 
-console.log(MyInfo.name);
+window.addEventListener("resize", handleResize); // window의 크기를 바꾸면 handleResize를 호출, handleResize() 를 넣으면 resize 이벤트 상관없이 함수 시행
 
-MyInfo.name = "HuhHanWoo";
 
-console.log(MyInfo.name);
-
-function sayHello(name){
-    console.log(name + 'Hello');
+function handleClick(){
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick); // title을 클릭하면 색깔 change
+}
+
+init();
